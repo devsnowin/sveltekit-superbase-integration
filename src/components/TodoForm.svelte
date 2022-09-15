ef2e76a3b145fe271df0308663f2fffa0806a223
devsnowin <20103104@hit.edu.in>
2022-09-16 00:26:50 +0530

    added required field to the todo input field

--- src/components/TodoForm.svelte
+++ src/components/TodoForm.svelte
@@ -12,6 +12,6 @@
 </script>
 
 <form class="w-full flex justify-between items-center gap-2" on:submit|preventDefault={handleSubmit}>
-    <input type="text" class="rounded p-2 w-4/5 text-gray-900 focus:outline outline-offset-2 outline-4 outline-green-400" bind:value={todo} name="todo" placeholder="eg:buy milk">
+    <input type="text" class="rounded p-2 w-4/5 text-gray-900 focus:outline outline-offset-2 outline-4 outline-green-400" bind:value={todo} name="todo" placeholder="eg:buy milk" required>
     <input type="submit" class="bg-green-500 p-2 rounded w-1/5" value="add">
 </form>
